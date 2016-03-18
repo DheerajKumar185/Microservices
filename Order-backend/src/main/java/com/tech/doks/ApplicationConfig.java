65a42f15b1784d303719030345691412a7b969dc
DheerajKumar185 <dheerajkumar147@gmail.com>
2016-03-18 14:24:30 +0530

    Initial Version.

---
+++ Order-backend/src/main/java/com/tech/doks/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package com.tech.doks;
+
+import javax.inject.Named;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationConfig {
+	@Named
+	static class JerseyConfig extends ResourceConfig {
+		public JerseyConfig() {
+			this.packages("com.tech.doks.rest");
+		}
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate;
+	}
+}
